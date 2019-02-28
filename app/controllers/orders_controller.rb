e65b94113fa52924b5897017ad031d98d9aa4c3a
fahim-abdullah <fahimabdullahh@gmail.com>
2019-02-28 12:18:24 +0800

    documents and locate pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
+	def index
+		@orders = Order.all
+	end
+
 	def new
 		@order = Order.new
 	end
@@ -16,9 +20,26 @@ class OrdersController < ApplicationController
 		@order = Order.find(params[:id])
 	end
 
-	private
-	def order_params 
-		params.require(:order).permit(:prefix, :awbnum, :queue)
+	def documents 
+		@order = Order.find(params[:order_id])
 	end
 
+	def locate
+		@order = Order.find(params[:order_id])
+	end
+
+	def update 
+		@order = Order.find(params[:id])
+		if @order.update(order_params)
+			flash[:notice] = "Updated"
+			redirect_to order_path(@order)
+		else
+			# render 'documents'
+		end
+	end
+
+	private
+	def order_params 
+		params.require(:order).permit(:prefix, :awbnum, :terminal_charge, :pieces, :perishable_status, :pieces_found, :partial_release, :located_time, :arranged_time, :delivered_by, :delivery_time)
+	end
 end
