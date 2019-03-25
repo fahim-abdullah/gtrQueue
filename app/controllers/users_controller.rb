1d32530c86017c55fec11c570a0ae43cc1e07b22
fahim-abdullah <fahimabdullahh@gmail.com>
2019-03-25 14:19:43 +0800

    create admin from ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params 
-		params.require(:user).permit(:username, :password)
+		params.require(:user).permit(:username, :password, :admin)
 	end
 	def set_timezone
 		Time.zone = "Kuala Lumpur"
